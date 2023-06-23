4f0017729ab15e6a1d2166500c22cdb3316c9a05
FaustVX <jodu035@gmail.com>
2023-06-23 22:25:25 +0200

    copy-pastable .vscode tasks files

--- .vscode/zip.ps1
+++ .vscode/zip.ps1
@@ -1,11 +1,13 @@
+$zip = Get-Item "$($args[0]).zip"
+
 try
 {
-    if ((Get-ChildItem -Recurse -File -Exclude *.zip,.vscode\* .\ | Where{$_.LastWriteTime -ge (Get-Item .\MakeSuspicious.zip).LastWriteTime}).Count -gt 0)
+    if ((Get-ChildItem -Recurse -File -Exclude *.zip,.vscode\* .\ | Where{$_.LastWriteTime -ge $zip.LastWriteTime}).Count -gt 0)
     {
-        C:\"Program Files"\7-Zip\7z.exe a MakeSuspicious.zip data pack.mcmeta
+        C:\"Program Files"\7-Zip\7z.exe a "$($args[0]).zip" data pack.mcmeta
     }
 }
 catch
 {
-    C:\"Program Files"\7-Zip\7z.exe a MakeSuspicious.zip data pack.mcmeta
+    C:\"Program Files"\7-Zip\7z.exe a "$($args[0]).zip" data pack.mcmeta
 }
